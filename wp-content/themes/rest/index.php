34c23eb8ddefabb828465dd1fbd08f3514eb8185
André Tashiro <andre_seichi@hotmail.com>
2021-02-19 16:01:32 -0300

    função loop básica para mostrar os posts

--- wp-content/themes/rest/index.php
+++ wp-content/themes/rest/index.php
@@ -1,5 +1,15 @@
 <?php get_header(); ?>
+<!-- Início loop posts -->
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-<p>asodjadaisa</p>
+        <h1><?php the_title(); ?></h1>
+        <?php the_content(); ?>
 
+        <!-- Fim do loop posts -->
+    <?php endwhile;
+else : ?>
+    <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+<?php endif; ?>
+
+<p>aqui está fora do loop</p>
 <?php get_footer(); ?>

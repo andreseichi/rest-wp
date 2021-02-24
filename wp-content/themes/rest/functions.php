37226a134d626222199f0637ad12966da9282004
André Tashiro <andre_seichi@hotmail.com>
2021-02-24 20:51:07 -0300

    adicionando campo no WP para arquivo

--- wp-content/themes/rest/functions.php
+++ wp-content/themes/rest/functions.php
@@ -14,9 +14,8 @@ function the_field_cmb2($key, $page_id = 0)
     echo get_field_cmb2($key, $page_id);
 }
 
-
+// Função para adicionar o campo no WP da página home
 add_action('cmb2_admin_init', 'cmb2_fields_home');
-
 function cmb2_fields_home()
 {
     // Criando um bloco
@@ -43,3 +42,32 @@ function cmb2_fields_home()
         'type' => 'textarea',
     ]);
 }
+
+
+// Função para adicionar o campo no WP da página sobre
+add_action('cmb2_admin_init', 'cmb2_fields_sobre');
+function cmb2_fields_sobre()
+{
+    // Criando um bloco
+    $cmb = new_cmb2_box([
+        'id' => 'sobre_box',
+        'title' => 'Campo personalizado do Sobre',
+        'object_types' => ['page'],
+        'show_on' => [
+            'key' => 'page-template',
+            'value' => 'page-sobre.php'
+        ],
+    ]);
+
+    // Campo arquivo
+    $cmb->add_field([
+        'name' => 'Foto Rest',
+        'id' => 'foto_rest',
+        'type' => 'file',
+        'options' => [
+            'url' => true,
+        ],
+    ]);
+}
+
+?>
